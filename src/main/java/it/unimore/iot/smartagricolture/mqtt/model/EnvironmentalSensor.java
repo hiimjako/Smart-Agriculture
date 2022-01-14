918ebb37e4eec51c02351b6afce2ea9cfc8202df
hiimjako <moretti919@gmail.com>
2022-01-14 13:24:09 +0100

    Added SenML rappresentation of environmentSensor

--- src/main/java/it/unimore/iot/smartagricolture/mqtt/model/EnvironmentalSensor.java
+++ src/main/java/it/unimore/iot/smartagricolture/mqtt/model/EnvironmentalSensor.java
@@ -1,6 +1,9 @@
 package it.unimore.iot.smartagricolture.mqtt.model;
 
+import it.unimore.iot.smartagricolture.mqtt.model.interfaces.ISenMLFormat;
 import it.unimore.iot.smartagricolture.mqtt.model.sensor.*;
+import it.unimore.iot.smartagricolture.mqtt.utils.SenMLPack;
+import it.unimore.iot.smartagricolture.mqtt.utils.SenMLRecord;
 
 /**
  * @author Alberto Moretti, 272804@studenti.unimore.it
@@ -8,7 +11,7 @@ import it.unimore.iot.smartagricolture.mqtt.model.sensor.*;
  * @project smart-agriculture
  * @created 02/01/2022 - 16:18
  */
-public class EnvironmentalSensor extends SmartObjectBase {
+public class EnvironmentalSensor extends SmartObjectBase implements ISenMLFormat {
     private Temperature temperatureSensor = new Temperature();
     private Brightness brightnessSensor = new Brightness();
     private Humidity humiditySensor = new Humidity();
@@ -19,7 +22,7 @@ public class EnvironmentalSensor extends SmartObjectBase {
     }
 
 
-    public EnvironmentalSensor(double temperature, double brightness, double humidity, double rainLevel, int batteryPercentage) {
+    public EnvironmentalSensor(double temperature, double brightness, double humidity, boolean rainLevel, int batteryPercentage) {
         this.temperatureSensor = new Temperature(temperature);
         this.brightnessSensor = new Brightness(brightness);
         this.humiditySensor = new Humidity(humidity);
@@ -57,5 +60,48 @@ public class EnvironmentalSensor extends SmartObjectBase {
                 ", rainSensor=" + rainSensor +
                 ", battery=" + battery +
                 '}';
+    }
+
+    @Override
+    public SenMLPack toSenML(Object object) {
+        SenMLPack senMLPack = new SenMLPack();
+
+        SenMLRecord senMLRecord = new SenMLRecord();
+        //battery
+        senMLRecord.setBn(this.getId());
+        senMLRecord.setBt(System.currentTimeMillis());
+        senMLRecord.setN(Battery.SENML_NAME);
+        senMLRecord.setU(Battery.SENML_UNIT);
+        senMLRecord.setV(this.battery.getBatteryPercentage());
+        senMLPack.add(senMLRecord);
+
+        senMLPack = new SenMLPack();
+        //temperatureSensor
+        senMLRecord.setN(Temperature.SENML_NAME);
+        senMLRecord.setU(Temperature.SENML_UNIT);
+        senMLRecord.setV(this.temperatureSensor.getValue());
+        senMLPack.add(senMLRecord);
+
+        senMLPack = new SenMLPack();
+        //brightnessSensor
+        senMLRecord.setN(Brightness.SENML_NAME);
+        senMLRecord.setU(Brightness.SENML_UNIT);
+        senMLRecord.setV(this.brightnessSensor.getValue());
+        senMLPack.add(senMLRecord);
+
+        senMLPack = new SenMLPack();
+        //humiditySensor
+        senMLRecord.setN(Humidity.SENML_NAME);
+        senMLRecord.setU(Humidity.SENML_UNIT);
+        senMLRecord.setV(this.humiditySensor.getValue());
+        senMLPack.add(senMLRecord);
+
+        senMLPack = new SenMLPack();
+        //rainSensor
+        senMLRecord.setN(Rain.SENML_NAME);
+        senMLRecord.setVb(this.rainSensor.isRaining());
+        senMLPack.add(senMLRecord);
+
+        return senMLPack;
     }
 }
